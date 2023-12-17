3bec9638b363b11bb7d5451923c713a572794c4b
Wtz_LASR <wtz_lasr@outlook.com>
2023-12-17 08:27:41 +0800

    implement parser driver

---
+++ parser/src/zombie/tvirus/parser/Driver.scala
@@ -0,0 +1,50 @@
+package zombie.tvirus.parser
+
+import zombie.tvirus.parser.generated.*
+import TVirusParser.*
+import org.antlr.v4.runtime.CharStream
+import org.antlr.v4.runtime.Vocabulary
+import org.antlr.v4.runtime.CommonTokenStream
+
+class TVirusExprVisitor extends TVirusBaseVisitor[Expr] {
+  override def visitCalculation(ctx: CalculationContext): Expr =
+    Expr.Calc(
+      visit(ctx.expr(1)),
+      ctx.op.getType() match {
+        case TVirusLexer.MINUS => Op.MINUS
+        case TVirusLexer.MULT  => Op.MULT
+        case TVirusLexer.PLUS  => Op.PLUS
+      },
+      visit((ctx.expr(2)))
+    )
+
+  override def visitParen(ctx: ParenContext): Expr = visit(ctx.expr())
+
+  override def visitApplication(ctx: ApplicationContext): Expr =
+    Expr.App(visit(ctx.expr(0)), visit(ctx.expr(1)))
+
+  override def visitAbstraction(ctx: AbstractionContext): Expr =
+    Expr.Abs(ctx.VAR().getSymbol().getText(), visit(ctx.expr()))
+
+  override def visitVariable(ctx: VariableContext): Expr =
+    Expr.Var(ctx.VAR().getSymbol().getText())
+
+  override def visitInteger(ctx: IntegerContext): Expr =
+    Expr.Int(Integer.parseInt(ctx.INT().getSymbol().getText()))
+
+  override def visitAbstractionRec(ctx: AbstractionRecContext): Expr =
+    Expr.AbsRec(
+      ctx.VAR(0).getSymbol().getText(),
+      ctx.VAR(1).getSymbol().getText(),
+      visit(ctx.expr())
+    )
+}
+
+private def visitorInstance = TVirusExprVisitor()
+
+def drive(source: CharStream): Expr = {
+    val lexer = TVirusLexer(source)
+    val stream = CommonTokenStream(lexer)
+    val parser = TVirusParser(stream)
+    visitorInstance.visit(parser.expr())
+}
