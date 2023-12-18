76e1d8c29b2d1e33d65f9ef68a8e476dc577d243
简 <2447746158@qq.com>
2023-12-18 23:33:49 +0800

    add Enums.Statement

--- parser/src/zombie/tvirus/parser/Enums.scala
+++ parser/src/zombie/tvirus/parser/Enums.scala
@@ -3,9 +3,18 @@ package zombie.tvirus.parser
 enum Op:
     case PLUS, MINUS, MULT
 
+enum Type:
+    case Int()
+    case Func(from: Type, target: Type)
+    case DataType(name: String)
+
 enum Expr:
     case Var(id: String)
     case Int(int: Integer)
     case Calc(l: Expr, op: Op, r: Expr)
     case App(f: Expr, x: Expr)
     case Abs(x: String, body: Expr)
+
+enum Statement:
+    case Let(name: String, expr: Expr)
+    case Data(name: String, unionFields: List[(String, List[Type])])
