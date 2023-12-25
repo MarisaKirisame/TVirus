e21b147cfa1efeb6e34cadd553efbd37e922fad6
Wtz_LASR <wtz_lasr@outlook.com>
2023-12-25 14:45:04 +0800

    some tyck defs

--- codegen/src/zombie/tvirus/codegen/passes/Tyck.scala
+++ codegen/src/zombie/tvirus/codegen/passes/Tyck.scala
@@ -1,9 +1,24 @@
 package zombie.tvirus.codegen.passes
 
 import cats.collections.DisjointSets
-import zombie.tvirus.parser.{Expr as PExpr, Type as PType, Scheme as PScheme}
+import zombie.tvirus.parser as p
+import cats.kernel.Order
 
-case class AExpr(e: Expr, t: Type)
+case class AExpr(e: Expr, t: p.Type)
 
 enum Expr:
   case Var(name: String)
+
+case class Constraint(name: String, ty: p.Type)
+
+given Order[Constraint] = new Order[Constraint] {
+  override def compare(x: Constraint, y: Constraint): Int =
+    Order[String].compare(x.name, y.name)
+}
+
+case class Gamma(
+    typeDecls: Seq[p.TypeDecl],
+    terms: Map[String, AExpr],
+    constraints: DisjointSets[Constraint]
+)
+
