42bfcb18246cb6d9b4538a2b42776ddb8f4872e1
Wtz_LASR <wtz_lasr@outlook.com>
2024-01-08 05:30:36 +0800

    fix cli

--- cli/src/zombie/tvirus/cli/Main.scala
+++ cli/src/zombie/tvirus/cli/Main.scala
@@ -7,32 +7,29 @@ import mainargs.{main, arg, ParserForMethods, Flag}
 import zombie.tvirus.parser.drive
 import zombie.tvirus.codegen.{compile, codegen}
 
-object Main{
+object Main {
   @main
   def run(
-    @arg(positional = true, doc = "source-file")
-    source_file: String,
-    @arg(short = 'o', doc = "target-file")
-    target_file: String = "a.cpp"
+      @arg(positional = true, doc = "source-file")
+      source_file: String,
+      @arg(short = 'o', doc = "target-file")
+      target_file: String = "a.cpp"
   ) = {
     val fileReader = new FileReader(new File(source_file))
 
     val prog = drive(CharStreams.fromReader(fileReader))
 
-    compile(prog) match
-    case Left(msg) => println(msg)
-    case Right(core) => {
-      try {
-        val fileWriter = new FileWriter(target_file)
+    try {
+      val fileWriter = new FileWriter(target_file)
 
-        fileWriter.write(codegen(core))
+      fileWriter.write(codegen(prog))
 
-        fileWriter.close()
-      } catch {
-        case e: IOException =>
-          println("fail to write into target-file" + e.getMessage)
-      }
+      fileWriter.close()
+    } catch {
+      case e: IOException =>
+        println("fail to write into target-file" + e.getMessage)
     }
+
   }
   def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
 }
