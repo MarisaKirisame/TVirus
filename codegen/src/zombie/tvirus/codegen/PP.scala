c64ce7fb58352ddbeea92ad284e3e115d4c5407a
神楽一葉 <1310652199@qq.com>
2024-01-18 14:07:06 -0500

    prettier

--- codegen/src/zombie/tvirus/codegen/PP.scala
+++ codegen/src/zombie/tvirus/codegen/PP.scala
@@ -12,19 +12,7 @@ def cbracket(x: String) = {
   "{" + x + "}"
 }
 
-def pp_type(x: Type): /* String */ Description = {
-  /*
-  resolve(x) match {
-    case Type.Var(name, _) => name
-    case Type.App(f, x)    => bracket(pp_type(f) + bracket(x.map(pp_type).mkString(", ")))
-    case Type.Func(l, r) =>
-      "(" + l.map(pp_type).mkString(", ") + ") -> " + pp_type(r)
-    case Type.TyCons(x) => x
-    case Type.TypeScheme(xs, y) => s"forall ${xs.mkString(" ")}, ${pp_type(y)}"
-    case Type.Prim(PrimType.INT) => "int"
-    case Type.Prim(PrimType.BOOL) => "bool"
-  }
-  */
+def pp_type(x: Type): Description = {
   resolve(x) match {
     case Type.Var(name, _) => Text(name)
     case Type.App(f, y) =>
@@ -39,27 +27,14 @@ def pp_type(x: Type): /* String */ Description = {
 }
 
 def pp_cbind(x: CBind): Description = {
-  /*
-  x.name + " " + x.args.map(pp_type).mkString(" ")
-  */
   Text(x.name) <> Text(" ") <> x.args.map(pp_type).flatMap(d => List(d, Text(" "))).dropRight(1).foldLeft(Text(""))(Concat(_, _))
 }
 
 def pp_typedecl(x: TypeDecl): Description = {
-  /*
-  x.name + " " + x.xs
-    .mkString(",") + " = " + x.cons.map(pp_cbind).mkString("\n| ")
-  */
   Text(x.name) <> Text(" ") <> Text(x.xs.mkString(",")) <> Text(" = ") <> Text(x.cons.map(pp_cbind).mkString("\n| "))
 }
 
 def pp_pat(x: Pat): Description = {
-  /*
-  x match
-    case Pat.Wildcard         => "_"
-    case Pat.Cons(name, args) => s"$name " + args.map(pp_pat(_)).mkString(" ")
-    case Pat.Var(x)           => x
-  */
   x match
     case Pat.Wildcard => Text("_")
     case Pat.Cons(name, args) => Text(s"$name " + args.map(pp_pat(_)).mkString(" "))
@@ -67,21 +42,10 @@ def pp_pat(x: Pat): Description = {
 }
 
 def pp_binding(bind: (String, Expr)): Description = {
-  /*
-  bind(0) + " = " + pp_expr(bind(1))
-  */
   Text(bind(0)) <> Text (" = ") <> pp_expr(bind(1))
 }
 
 def pp_op(op: PrimOp): Description = {
-  /*
-  op match
-    case PrimOp.EQ => "=="
-    case PrimOp.ADD => "+"
-    case PrimOp.MINUS => "-"
-    case PrimOp.LT => "<"
-    case PrimOp.GT => ">"
-  */
   op match
     case PrimOp.EQ => Text("==")
     case PrimOp.ADD => Text("+")
@@ -91,26 +55,6 @@ def pp_op(op: PrimOp): Description = {
 }
 
 def pp_expr(x: Expr): Description = {
-  /*
-  x match {
-    case Expr.Abs(xs, b) =>
-      bracket("\\" + xs.mkString(" ") + " -> " + pp_expr(b))
-    case Expr.App(f, x) =>
-      bracket(pp_expr(f) + "(" + x.map(pp_expr).mkString(", ") + ")")
-    case Expr.Var(x) => x
-    case Expr.Match(x, cases) =>
-      "(match " + pp_expr(x) + " with " + cases
-        .map(y => pp_pat(y(0)) + " => " + pp_expr(y(1)))
-        .mkString("\n| ") + ")"
-    case Expr.Cons(con, xs) => con + "(" + xs.map(pp_expr).mkString(", ") + ")"
-    case Expr.Let(binding, body) =>
-      "let " + binding.map(pp_binding).mkString(", ") + " in " + pp_expr(body)
-    case Expr.LitInt(x) => x.toString()
-    case Expr.Prim(l, op, r) => s"(${pp_expr(l)} ${pp_op(op)} ${pp_expr(r)})"
-    case Expr.If(i, t, e) => s"if ${pp_expr(i)} {${pp_expr(t)}} else {${pp_expr(e)}}"
-    case Expr.Fail() => "fail"
-  }
-  */
   x match
     case Expr.Abs(xs, b) =>
       Text("(") <> Text("\\") <> Text(xs.mkString(" ")) <> Text(" -> ") <> pp_expr(b) <> Text(")")
@@ -131,16 +75,10 @@ def pp_expr(x: Expr): Description = {
 }
 
 def pp_valdecl(x: ValueDecl): Description = {
-  /*
-  x.x + " = " + pp_expr(x.b)
-  */
   Text(x.x) <> Text(" = ") <> pp_expr(x.b)
 }
 
-def pp(x: Program) = {
-  /*
-  x.tds.map(pp_typedecl).mkString("\n") + "\n" + x.vds.map(pp_valdecl).mkString("\n")
-  */
+def pp(x: Program): Description = {
   x.tds.map(pp_typedecl).flatMap(d => List(d, Text("\n"))).dropRight(1).foldLeft(Text(""))(Concat(_, _)) <> Text("\n") <>
     x.vds.map(pp_valdecl).flatMap(d => List(d, Text("\n"))).dropRight(1).foldLeft(Text(""))(Concat(_, _))
 }
