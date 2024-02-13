2786a257d0c90cc0dfb473ca1508b0f1fe436c7a
Marisa Kirisame <marisa@cs.utah.edu>
2024-02-13 01:23:22 -0700

    save

--- anal.py
+++ anal.py
@@ -6,6 +6,12 @@ import os
 import sys
 import numpy as np
 import math
+import dominate
+from dominate.tags import *
+import subprocess
+
+out_path = "output/" + get_time()
+os.mkdir(out_path)
 
 aggregate = {}
 
@@ -25,6 +31,32 @@ def plot(result):
     plt.xlabel("time (seconds)")
     plt.ylabel("space (MB)")
 
+def lr(points):
+    points.sort()
+    x, y = zip(*points)
+
+    coef = np.polyfit([math.log(x_) for x_ in x], [math.log(y_) for y_ in y], 1)
+    poly1d_fn = np.poly1d(coef)
+
+    plt.scatter(1, 1, label="baseline")
+    plt.plot(x, [math.exp(y_) for y_ in poly1d_fn([math.log(x_) for x_ in x])], '--k') #'--k'=black dashed line, 'yo' = yellow circle marker
+
+    p(f"10x space = {round(math.exp(poly1d_fn(math.log(1)) - poly1d_fn(math.log(10))), 3)}x time!")
+
+COUNTER = 0
+def count():
+    global COUNTER
+    ret = COUNTER
+    COUNTER += 1
+    return ret
+
+def flush_plt():
+    rel_path = str(count()) + ".png"
+    plt.savefig(f"{out_path}/{rel_path}")
+    plt.close()
+    img(src=rel_path)
+    br()
+
 def work(path):
     runs = []
 
@@ -38,64 +70,63 @@ def work(path):
     baseline_space = None
 
     for r in runs:
-        print(r.config)
         if r.config["backend"]["name"] == "baseline":
             baseline_space, baseline_time = get_spacetime(r)
 
     zombie_points = []
     baseline_points = []
 
-    for r in runs:
-        space, time = get_spacetime(r)
-        print(f"space = {space/1e6}MB, time = {time/1e9}s")
-        space = space / baseline_space
-        time = time / baseline_time
-        if r.config["backend"]["name"] == "zombie":
-            zombie_points.append((space, time))
-        else:
-            baseline_points.append((space, time))
-            #space, time = get_spacetime(r, use_total_allocated=True)
-            #print(f"space = {space/1e6}MB, time = {time/1e9}s")
-            #space = space / zombie_baseline_space
-            #time = time / zombie_baseline_time
-            #baseline_points.append((space, time))
+    doc = dominate.document(title='Dominate your HTML')
+    with doc:
+        for r in runs:
+            space, time = get_spacetime(r)
+            p(f"space = {space/1e6}MB, time = {time/1e9}s")
+            space = space / baseline_space
+            time = time / baseline_time
+            if r.config["backend"]["name"] == "zombie":
+                zombie_points.append((space, time))
+            else:
+                baseline_points.append((space, time))
+                #space, time = get_spacetime(r, use_total_allocated=True)
+                #print(f"space = {space/1e6}MB, time = {time/1e9}s")
+                #space = space / zombie_baseline_space
+                #time = time / zombie_baseline_time
+                #baseline_points.append((space, time))
 
-    assert res.config["program"] not in aggregate
-    aggregate[res.config["program"]] = (baseline_points, zombie_points)
+        assert res.config["program"] not in aggregate
+        aggregate[res.config["program"]] = (baseline_points, zombie_points)
 
-    if len(zombie_points) != 0:
-        x, y = zip(*zombie_points)
-        plt.scatter(x, y, label="zombie")
+        if len(zombie_points) != 0:
+            x, y = zip(*zombie_points)
+            plt.scatter(x, y, label="zombie")
 
-        coef = np.polyfit(x, [1/v for v in y], 1)
-        poly1d_fn = np.poly1d(coef)
+            lr(zombie_points)
 
-        plt.plot(sorted(x), 1/poly1d_fn(sorted(x)), '--k') #'--k'=black dashed line, 'yo' = yellow circle marker
+        if len(baseline_points) != 0:
+            x, y = zip(*baseline_points)
+            plt.scatter(x, y, label="baseline")
 
-    if len(baseline_points) != 0:
-        x, y = zip(*baseline_points)
-        plt.scatter(x, y, label="baseline")
+        plt.xscale('log')
+        plt.xlabel("space")
 
-    plt.xscale('log')
-    plt.xlabel("space")
+        plt.yscale('log')
+        plt.ylabel("time")
 
-    plt.yscale('log')
-    plt.ylabel("time")
+        plt.legend()
+        plt.title(res.config["program"])
+        flush_plt()
 
-    plt.legend()
-    plt.savefig("./result.png")
-    plt.title(res.config["program"])
-    #plt.show()
-    plt.close()
+    return (res.config["program"], doc)
 
 if len(sys.argv) > 1:
     d = sys.argv[1]
 else:
     d = "log/" + sorted(os.listdir("log"))[-1]
 
+subpages = []
 for x in os.listdir(d):
     if os.path.isdir(f"{d}/{x}"):
-        work(f"{d}/{x}")
+        subpages.append(work(f"{d}/{x}"))
 
 zombie_aggregate = []
 for k in aggregate:
@@ -111,25 +142,25 @@ for k in aggregate:
     #    x, y = zip(*baseline_points)
     #    plt.scatter(x, y, label="baseline")
 
-zombie_aggregate.sort()
-x, y = zip(*zombie_aggregate)
+doc = dominate.document(title='Dominate your HTML')
 
-coef = np.polyfit([math.log(x_) for x_ in x], [math.log(y_) for y_ in y], 1)
-poly1d_fn = np.poly1d(coef)
+with doc:
+    lr(zombie_aggregate)
 
+    plt.xscale('log')
+    plt.xlabel("space")
 
-print(f"10x space = {math.exp(poly1d_fn(math.log(1)) - poly1d_fn(math.log(10)))}x time!")
+    plt.yscale('log')
+    plt.ylabel("time")
 
-plt.scatter(1, 1, label="baseline")
-plt.plot(x, [math.exp(y_) for y_ in poly1d_fn([math.log(x_) for x_ in x])], '--k') #'--k'=black dashed line, 'yo' = yellow circle marker
+    plt.legend()
+    flush_plt()
 
-plt.xscale('log')
-plt.xlabel("space")
+    for program_name, page in subpages:
+        rel_path = str(count()) + ".html"
+        write_to(f"{out_path}/{rel_path}", str(page))
+        a(program_name, href=rel_path)
+        br()
 
-plt.yscale('log')
-plt.ylabel("time")
-
-plt.legend()
-plt.savefig("./result.png")
-plt.show()
-plt.close()
+write_to(out_path + "/index.html", str(doc))
+subprocess.run(f"xdg-open {out_path}/index.html", shell=True, check=True)
